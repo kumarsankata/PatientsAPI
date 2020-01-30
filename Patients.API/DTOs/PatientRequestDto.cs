049dd4bcab00f3c4d994fcbf3acb541983f28c82
PPETSURE\kumars <kumars@petsure.com.au>
2020-01-30 18:03:08 +1100

    Added Patient Request and Response DTOs

---
+++ Patients.API/DTOs/PatientRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patients.API.DTOs
+{
+    public class PatientRequestDto
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime Date_Of_Birth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Is_Active { get; set; }      
+    }
+}
