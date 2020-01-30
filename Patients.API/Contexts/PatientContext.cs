20fc47f7aff64e63106536d744469469ac0d31bc
PPETSURE\kumars <kumars@petsure.com.au>
2020-01-30 18:36:08 +1100

    Added DBContext class

---
+++ Patients.API/Contexts/PatientContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patients.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patients.API.Contexts
+{
+    public class PatientContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
