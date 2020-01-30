32d8cf208b6e5cf0e34a140fab4e06a90608b201
PPETSURE\kumars <kumars@petsure.com.au>
2020-01-30 18:34:10 +1100

    Added a Patient Entity

---
+++ Patients.API/Entities/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patients.API.Entities
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime Date_Of_Birth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Is_Active { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+    }
+}
