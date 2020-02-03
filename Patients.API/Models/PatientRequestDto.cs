3f3017faa78b8905d92bd1d31deaae4509c922ca
PPETSURE\kumars <kumars@petsure.com.au>
2020-02-03 23:23:11 +1100

    added more validations

--- Patients.API/Models/PatientRequestDto.cs
+++ Patients.API/Models/PatientRequestDto.cs
@@ -6,17 +6,27 @@ namespace Patients.API.Models
     public class PatientRequestDto
     {
         [Required]
+        [MaxLength(50)]
         public string First_Name { get; set; }
+
         [Required]
+        [MaxLength(50)]
         public string Last_Name { get; set; }
+
         [Required]
         public string Gender { get; set; }
+
         [Required]
         public DateTime Date_Of_Birth { get; set; }
+
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
         [Required]
+        [Phone]
         public string Phone { get; set; }
+
         [Required]
         public bool Is_Active { get; set; }      
     }
